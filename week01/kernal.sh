7531161a99018fb3719525d8a7233e37e8cd14de
soro <1ms22cs126@msrit.edu>
2025-03-06 10:43:21 +0530

    Update and rename run.sh to week01/kernal.sh

--- week01/kernal.sh
+++ week01/kernal.sh
@@ -1,11 +1,11 @@
-textbook: https://www.doc-developpement-durable.org/file/Projets-informatiques/cours-&-manuels-informatiques/Linux/Linux%20Kernel%20Development,%203rd%20Edition.pdf
+# textbook: https://www.doc-developpement-durable.org/file/Projets-informatiques/cours-&-manuels-informatiques/Linux/Linux%20Kernel%20Development,%203rd%20Edition.pdf
 
 tar -xvf linux-6.13.5.tar.xz
 cd linux-6.13.5/
 bison --version // 3.7+
 
 
-bison: make sure to do in linux-6.13.5/ directory
+# bison: make sure to do in linux-6.13.5/ directory
 wget http://ftp.gnu.org/gnu/bison/bison-3.8.2.tar.gz
 tar -xvzf bison-3.8.2.tar.gz
 cd bison-3.8.2
@@ -20,5 +20,5 @@ bison --version // 3.7+
 
 
 
-continue.. 
+# continue.. 
  make defconfig
